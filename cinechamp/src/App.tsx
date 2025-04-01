c367fb4fe7e7a1b46920a1aed67a85401246d105
cris-ubuntu <cdcalderonrojas@gmail.com>
2025-04-01 16:45:24 +0200

    Rutas agregadas

--- cinechamp/src/App.tsx
+++ cinechamp/src/App.tsx
@@ -1,41 +1,19 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import imgperfil from './assets/images-perfil/perfil-example.jpg';
+// App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Search from './pages/Search';
 
-import Profile from './user-profile/profile';
-import PFavorites from './p-favoritos/favoritos';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState<any>(null);  // Aquí almacenaremos los datos del backend
-
-  useEffect(() => {
-    // Hacemos la petición fetch al backend
-    fetch('http://localhost:5000/data') // Cambia la URL según tu backend
-      .then(response => response.json())
-      .then(data => {
-        setData(data); // Actualizamos el estado con los datos recibidos
-      })
-      .catch(error => console.error('Error al obtener los datos:', error));
-  }, []);
-
   return (
-    <>
-      <div className="App">
-        <Profile img={imgperfil} nick="Juan" ubicacion="Barcelona" grupo="Sin grupo" />
-        <PFavorites />
-        
-        {/* Mostrar los datos recibidos desde el backend */}
-        <div>
-          <h2>Datos del backend:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-        
-        {/* main */}
-
-        {/* etiqueta footer */}
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/search' element={<Search/>}/>
+      </Routes>
+    </Router>
   );
 }
 
